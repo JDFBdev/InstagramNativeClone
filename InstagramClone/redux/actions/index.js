8b95e47d52cea6565a5ac493f91fc6677510caf8
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-08-08 19:22:52 -0300

    User feed

--- InstagramClone/redux/actions/index.js
+++ InstagramClone/redux/actions/index.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import  { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from '../constants/index';
+import  { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USERS_POSTS_STATE_CHANGE } from '../constants/index';
 
 export function fetchUser(){
     return((dispatch)=>{
@@ -51,7 +51,56 @@ export function fetchUserFollowing(){
                     const id = doc.id;
                     return id;
                 })
-                dispatch({type: USER_FOLLOWING_STATE_CHANGE, payload: following})
+                dispatch({type: USER_FOLLOWING_STATE_CHANGE, payload: following});
+                for(let i = 0; i<following.length; i++){
+                    dispatch(fetchUsersData(following[i]));
+                }
+            })
+    })
+}
+
+export function fetchUsersData(uid){
+    return((dispatch, getState)=>{
+        const found = getState().users.some(el => el.uid === uid); // Checkeamos si el usuario ya esta cargado en el estado. Si no esta, lo buscamos
+        if(!found){
+            firebase.firestore()
+            .collection('users')
+            .doc(uid)
+            .get()
+            .then((snapshot)=>{
+                
+                if(snapshot.exists){
+                    let user = snapshot.data();
+                    user.uid = snapshot.uid;
+                    dispatch({type: USERS_DATA_STATE_CHANGE, payload: user});
+                    dispatch(fetchUsersFollowingPosts(user.id));
+                } else {
+                    console.log('User does not exist')
+                }
+            })
+        }
+    })
+}
+
+export function fetchUsersFollowingPosts(uid){
+    return((dispatch, getState)=>{
+        firebase.firestore()
+            .collection('posts')
+            .doc(uid)
+            .collection('userPosts')
+            .orderBy('creation', 'asc')
+            .get()
+            .then((snapshot)=>{
+                const uid = snapshot.docs[0].ref.path.split('/')[1];
+                const user = getState().users.find(el => el.uid === uid)
+
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return { id, user, ...data }
+                })
+                dispatch({type: USERS_POSTS_STATE_CHANGE, payload: {posts: posts, uid: uid}})
+                console.log(getState())
             })
     })
 }
--- InstagramClone/redux/reducers/index.js
+++ InstagramClone/redux/reducers/index.js
@@ -1,30 +1,50 @@
-import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE } from "../constants"
+import * as constants from "../constants"
 
 const initialState = {
     currentUser: null,
     posts: [],
-    following: []
+    following: [],
+    users: [],
+    usersLoaded: 0,
+    feed: []
 }
 
 export default function rootReducer(state = initialState, action){
 
   switch (action.type) {
-    case USER_STATE_CHANGE:
+    case constants.USER_STATE_CHANGE:
         return { 
             ...state,
             currentUser: action.payload
         }
 
-    case USER_POSTS_STATE_CHANGE:
+    case constants.USER_POSTS_STATE_CHANGE:
         return { 
             ...state,
             posts: action.payload
         }
 
-    case USER_FOLLOWING_STATE_CHANGE:
+    case constants.USER_FOLLOWING_STATE_CHANGE:
         return { 
             ...state,
             following: action.payload
+        }
+
+    case constants.USERS_DATA_STATE_CHANGE:
+        return { 
+            ...state,
+            users: [...state.users, action.payload]
+        }
+
+    case constants.USERS_POSTS_STATE_CHANGE:
+        return { 
+            ...state,
+            usersLoaded: state.usersLoaded + 1,
+            users: state.users.map(user => 
+                user.uid === action.payload.uid ? 
+                {...user, posts: action.payload.posts}:
+                user
+            )
         }
 
     default:
